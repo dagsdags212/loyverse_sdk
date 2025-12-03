b44477858862421ac30f039956de6d9dff77ee7a
dagsdags212 <jegsamson.dev@gmail.com>
2025-12-03 20:31:41 +0800

    import new models

--- src/loyverse_sdk/models/__init__.py
+++ src/loyverse_sdk/models/__init__.py
@@ -1,6 +1,7 @@
 from loyverse_sdk.models.discount import Discount, DiscountListResponse
 from loyverse_sdk.models.user import Customer, CustomerListResponse
 from loyverse_sdk.models.user import Employee, EmployeeListResponse
+from loyverse_sdk.models.merchant import Merchant
 from loyverse_sdk.models.receipt import (
     PaymentType,
     PaymentTypeListReponse,
@@ -11,3 +12,8 @@ from loyverse_sdk.models.device import PosDevice, PosDeviceListResponse
 from loyverse_sdk.models.store import Store, StoreListResponse
 from loyverse_sdk.models.item import Item, ItemListResponse
 from loyverse_sdk.models.category import Category, CategoryListResponse
+from loyverse_sdk.models.modifier import Modifier, ModifierListResponse
+from loyverse_sdk.models.supplier import Supplier, SupplierListResponse
+from loyverse_sdk.models.tax import Tax, TaxListResponse
+from loyverse_sdk.models.webhook import Webhook, WebhookListResponse
+from loyverse_sdk.models.variant import Variant, VariantListResponse
