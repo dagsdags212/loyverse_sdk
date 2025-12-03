6d7c0d6b79dc6a15e91ccc716991382ab7e96ddd
dagsdags212 <jegsamson.dev@gmail.com>
2025-12-03 14:32:30 +0800

    added helper function for standardizing datetime formats

--- src/loyverse_sdk/utils.py
+++ src/loyverse_sdk/utils.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from functools import wraps
 from typing import Type, Callable, Any
 from pydantic import BaseModel
+
+
+def standardize_datetime_str(dt: datetime) -> str:
+    """Coverts datetime object to ISO 8601 format"""
+    assert isinstance(dt, datetime), "dt must be a datetime object"
+    return dt.isoformat(timespec="milliseconds") + "Z"
 
 
 def convert_response(data: Any, model: Type[BaseModel]):
